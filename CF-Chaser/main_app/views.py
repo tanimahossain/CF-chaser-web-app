c38048be47e79ed360dd45e7181923e97b475bfc
Nazmus Sakib <nsakib800@gmail.com>
2021-04-11 00:36:47 +0600

    added login and registration pages and paths

--- CF-Chaser/main_app/views.py
+++ CF-Chaser/main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registration(request):
+
+    return render(request, 'registration.html')
+
+def logIn(request):
+
+    return render(request, 'login.html')
