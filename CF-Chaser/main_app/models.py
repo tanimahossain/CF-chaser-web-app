a0a64f853bd27cbe317457c3bea53f2f2d9fc01f
Nazmus Sakib <nsakib800@gmail.com>
2021-04-12 16:25:35 +0600

    models Profile and Friend created

--- CF-Chaser/main_app/models.py
+++ CF-Chaser/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    cfHandle = models.CharField(max_length=50, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.cfHandle
+
+class Friend(models.Model):
+    cfHandle = models.CharField(max_length=50, default=None)
+    friend_of = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cfHandle
